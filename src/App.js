79a142ac5495696b76a137f9f0115720df9a01a0
Dipak <ashutosh-chandra.dipak@capgemini.com>
2023-03-05 14:08:54 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Example from './features/othermodules/MaterialTableCRUD';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Hello from app</h1>
+     <Example></Example>
+    </div>
+  );
+}
+
+export default App;
