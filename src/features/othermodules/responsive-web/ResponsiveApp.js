727f200c5edf658835d080a8f6cd2740183963f7
Dipak <ashutosh-chandra.dipak@capgemini.com>
2023-03-12 13:01:36 +0530

    changes for aws

--- src/features/othermodules/responsive-web/ResponsiveApp.js
+++ src/features/othermodules/responsive-web/ResponsiveApp.js
@@ -4,21 +4,22 @@ import "./custom.css";
 export default function ResponsiveApp() {
   return (
     <div style={{fontFamily:"sans-serif",color:"#aaaaaa"}}>
+
       <div className="header">
         <h1>Header</h1>
       </div>
       <div className="allComp">
         <div className="menu">
-          <a href="#">Link 1</a>
-          <a href="#">Link 2</a>
-          <a href="#">Link 3</a>
-          <a href="#">Link 4</a>
+          <a href="#">Screen 1</a>
+          <a href="#">Screen 2</a>
+          <a href="#">Screen 3</a>
+          <a href="#">Screen 4</a>
         </div>
         <div className="main">
           <Example></Example>
         </div>        
       </div>
-      <div className="footer">© copyright w3schools.com</div>
+      <div className="footer">Footer for fuck</div>
     </div>
   );
 }
